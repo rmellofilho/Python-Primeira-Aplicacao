2d493ea2216e6c79957a0a648fd25cff32c6b293
Robeto Melo <rmello.filho@hotmail.com>
2024-02-07 18:56:38 -0300

    selecionando opções

--- app.py
+++ app.py
@@ -5,12 +5,21 @@ print("""
 ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
 ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
-"""))
+""")
 
 print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
-print('3. Ativar')
+print('3. Ativar restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
+opcao_escolhida = int(input('Escolha uma opção: '))
 print(f"Você escolheu a opção', {opcao_escolhida}")
+
+if(opcao_escolhida == 1):
+    print("Cadastrar restaurante")
+elif(opcao_escolhida == 2):
+    print("Listar restaurante")
+elif(opcao_escolhida == 3):
+    print("Ativar restaurante")
+else:
+    print("Encerrando o programa")
