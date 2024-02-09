25a7f74d2253def476f07e054623e6f85867bed5
Robeto Melo <rmello.filho@hotmail.com>
2024-02-09 18:58:24 -0300

    função opcao invalida

--- app.py
+++ app.py
@@ -25,6 +25,12 @@ def finalizar_app():
     print("Finalizando o app\n")
 
 
+def opcao_invalida():
+    print('Opção inválida!\n')
+    print('Digite uma tecla para voltar ao menu principal')
+    main()
+
+
 def escolher_opcao():
     opcao_escolhida = int(input('Escolha uma opção: '))
     print(f"Você escolheu a opção', {opcao_escolhida}")
@@ -35,11 +41,15 @@ def escolher_opcao():
         print("Listar restaurante")
     elif(opcao_escolhida == 3):
         print("Ativar restaurante")
-    else:
+    elif(opcao_escolhida == 4):
         finalizar_app()
+    else:
+        opcao_invalida()
+        
 
 
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
