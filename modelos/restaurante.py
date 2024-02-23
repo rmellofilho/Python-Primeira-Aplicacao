4f9972ddcee8f26961308e12043282a9d44c33f6
Robeto Melo <rmello.filho@hotmail.com>
2024-02-23 17:40:26 -0300

    ajustando status

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -8,24 +8,27 @@ class Restaurante:
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self.categoria}'
     
     @classmethod
     def listar_restaurantes(cls):
         print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante._ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
         return '☒' if self._ativo else '☐'
+    
+    def alternar_estado(self):
+        self._ativo= not self._ativo
+
 
 
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante('pizza express', 'Italiana')
-
-restaurantes = [restaurante_praca, restaurante_pizza]
 
 Restaurante.listar_restaurantes()
 
