4c74bc15b1099a32c02dc5326ef7979de01e68f5
Robeto Melo <rmello.filho@hotmail.com>
2024-02-23 18:14:15 -0300

    implementando avaliações para os restaurantes

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,3 +1,5 @@
+from modelos.avaliacao import Avaliacao
+
 class Restaurante:
     restaurantes = []
 
@@ -5,6 +7,7 @@ class Restaurante:
         self._nome = nome.title()
         self.categoria = categoria.upper()
         self._ativo = False
+        self._avaliacao = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
@@ -22,5 +25,10 @@ class Restaurante:
     
     def alternar_estado(self):
         self._ativo= not self._ativo
+
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacao.append(avaliacao)
+
 
 
