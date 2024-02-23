aac344c781b260493cdb9548f43f3a498cc34277
Robeto Melo <rmello.filho@hotmail.com>
2024-02-23 17:17:37 -0300

    Padronização de Nomes, Categorias em Restaurante Class e Class Method

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,23 +2,30 @@ class Restaurante:
     restaurantes = []
 
     def __init__ (self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self.categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
     
-    def listar_restaurantes():
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante._ativo}')
+
+    @property
+    def ativo(self):
+        return '☒' if self._ativo else '☐'
 
 
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza', 'Italiana')
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_pizza = Restaurante('pizza express', 'Italiana')
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
 Restaurante.listar_restaurantes()
+
